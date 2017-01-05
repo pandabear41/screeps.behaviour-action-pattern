dbd85d31a83fa5fb18f10c514f1362ea3ba6afd1
Eike Schuderer <eike.schuderer@sap.com>
2017-01-05 16:48:19 +0100

    Add mining task creep spawning logic

--- task.mining.js
+++ task.mining.js
@@ -24,9 +24,63 @@ var mod = {
         let room = Game.rooms[flag.pos.roomName];
         let memory = Task.mining.memory(flag);
 
+        let sources = room.find(FIND_SOURCES);
+
         // todo count creeps by type needed per source / mineral
         let haulerCount = null;
         let minerCount = null;
+
+        if(haulerCount < sources.length) {
+            for(var i = 0; i < sources.length; i++) {
+                let fixedBody = [MOVE, CARRY, CARRY, WORK];
+                let multiBody = [MOVE, CARRY, CARRY, CARRY];
+                let name = 'remoteHauler-' + flag.name;
+                let creep = {
+                    parts: Creep.Setup.compileBody(room, fixedBody, multiBody, true),
+                    name: name,
+                    setup: 'remoteHauler',
+                    destiny: { task: "mining", flagName: flag.name }
+                };
+                if( creep.parts.length === 0 ) {
+                    // creep has no body.
+                    global.logSystem(flag.pos.roomName, dye(CRAYON.error, 'Mining Flag tried to queue a zero parts body creep. Aborted.' ));
+                    return;
+                }
+                // queue creep for spawning
+                room.spawnQueueLow.push(creep);
+                // save queued creep to task memory
+                memory.queued.push({
+                    room: room.name,
+                    name: name
+                });
+            }
+        }
+
+        if(minerCount < sources.length * 2) {
+            for(var i = 0; i < sources.length * 2; i++) {
+                let fixedBody = [MOVE, MOVE, MOVE, WORK, WORK, WORK, WORK, WORK];
+                let multiBody = null;
+                let name = 'remoteMiner-' + flag.name;
+                let creep = {
+                    parts: Creep.Setup.compileBody(room, fixedBody, multiBody, true),
+                    name: name,
+                    setup: 'remoteMiner',
+                    destiny: { task: "mining", flagName: flag.name }
+                };
+                if( creep.parts.length === 0 ) {
+                    // creep has no body.
+                    global.logSystem(flag.pos.roomName, dye(CRAYON.error, 'Mining Flag tried to queue a zero parts body creep. Aborted.' ));
+                    return;
+                }
+                // queue creep for spawning
+                room.spawnQueueLow.push(creep);
+                // save queued creep to task memory
+                memory.queued.push({
+                    room: room.name,
+                    name: name
+                });
+            }
+        }
     }
     // todo: rest of task
 };
