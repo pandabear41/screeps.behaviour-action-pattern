65903bd4759359d1c0d35f816621094df84b81fb
Dan <askewholdings@gmail.com>
2017-01-09 21:30:33 +0000

    reserving

--- task.js
+++ task.js
@@ -3,13 +3,15 @@ var mod = {
     guard: load("task.guard"),
     defense: load("task.defense"),
     claim: load("task.claim"),
+    reserve: load("task.reserve"),
 
     // register tasks (hook up into events)
     register: function () {
         let tasks = [
             Task.guard, 
             Task.defense,
-            Task.claim
+            Task.claim,
+            Task.reserve
         ];
         var loop = task => {
             task.register();
